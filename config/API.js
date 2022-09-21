fb43acbd7328dee3524476d4455765c921a04d31
joonashendrik <55434493+joonashendrik@users.noreply.github.com>
2022-09-21 11:34:58 +0300

    Update API.js

--- config/API.js
+++ config/API.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-export const API = axios.create({
+select.addEventListener('input', (e) => log(e))
+
+const API = axios.create({
     baseURL: import.meta.env.VITE_RAPIDAPI_BASE_URL,
     headers:{
         'X-RapidAPI-Key': import.meta.env.VITE_X_RAPIDAPI_KEY,
         'X-RapidAPI-Host': import.meta.env.VITE_X_RAPIDAPI_HOST
     }
 })
+
 export default API;
